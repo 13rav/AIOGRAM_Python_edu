03f1da38e136250bff428b11891311f448602e1e
13rav <44643207+13rav@users.noreply.github.com>
2022-08-10 20:28:00 +0300

    12

---
+++ First.py
@@ -0,0 +1,14 @@
+from aiogramm import Bot, Dispatcher, executor, types
+
+TOKEN_API = '5371538285:AAGP91laa0Cbgs978G9-VnWS3DR3nv6FYz4'
+
+bot = Bot(TOKEN_API)
+dp = Dispatcher(bot)
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    sr = message.text + " автор"
+    await message.answer(text=sr)
+if __name__ == '__main__':
+    executor.start_polling(dp)
+
