ea253869ebcbb9f0289f35e340fab173997afe8e
Andrey <13rav.pr@gmail.com>
2022-08-21 00:42:49 +0300

    231

--- Lesson4_practise.py
+++ Lesson4_practise.py
@@ -11,10 +11,16 @@ TOKEN_API = '5371538285:AAGP91laa0Cbgs978G9-VnWS3DR3nv6FYz4'
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
 
-@dp.message_handler()
+@dp.message_handler(content_types=types.ContentTypes.ANY)
 async def description_message(message: types.Message):
-    if message.content_type!='photo':
-        await message.answer('Отправьте, пожалуйста, свою фотографию')
+    if message.content_type == ContentType.PHOTO:
+        await message.answer("я бот который становится умнее")
+
+    elif message.text:
+        await message.answer("Отправьте, пожалуйста, свою фотографию")
+
+    else:
+        message.answer("Это я не понимаю")
 
 
 if __name__ == '__main__':
