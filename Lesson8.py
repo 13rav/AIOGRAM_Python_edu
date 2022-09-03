5a9b63d7469246b90d439e4b4e8934cde62d67eb
Andrey <13rav.pr@gmail.com>
2022-09-03 19:27:05 +0300

    666

---
+++ Lesson8.py
@@ -0,0 +1,29 @@
+from email import message
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.types import ContentType, ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
+
+
+TOKEN_API = '5371538285:AAGP91laa0Cbgs978G9-VnWS3DR3nv6FYz4'
+
+bot = Bot(TOKEN_API)
+dp = Dispatcher(bot)
+
+HELP_COMMAND = """"Это тестовый бот, и он становится умнее
+
+<b>/sticker</b> отправляет стикер чык-чырыка
+<b>/photo</b> отправляет фото Пашы Дурова"""
+
+@dp.message_handler(commands=['help'])
+async def help_command(message: types.Message):
+    await message.answer(text=HELP_COMMAND, parse_mode='HTML')
+
+@dp.message_handler(commands=['sticker'])
+async def sticker_command(message: types.Message):
+    await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEFv3ljE3-1cl7bDAquCNiOLu4cc8bq9AACBAADoB7WCpwLfFk0SuWaKQQ')
+
+@dp.message_handler(commands=['photo'])
+async def photo_command(message: types.Message):
+    await bot.send_photo(message.from_user.id, photo='https://img.championat.com/news/big/j/n/durov-otkazalsja-ot-edy-i-alkogolja-poehala-krysha-ili-mozgi-vstali-na-mesto_15604007831083690418.jpg')
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
