b22beeb9d0b752d3c00a17f1e50f1faa90c7db78
Andrey <13rav.pr@gmail.com>
2022-09-07 18:33:12 +0300

    343q

---
+++ Lesson10.py
@@ -0,0 +1,36 @@
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.types import ContentType, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
+import random
+
+
+TOKEN_API = '5371538285:AAGP91laa0Cbgs978G9-VnWS3DR3nv6FYz4'
+
+HELP_COMMAND = """"Это тестовый бот, и он становится умнее
+
+<b>/sticker</b> отправляет стикер чык-чырыка
+<b>/photo</b> отправляет фото Паши Дурова
+<b>/location</b> отправляет случайную точку на карте"""
+
+
+bot = Bot(TOKEN_API)
+dp = Dispatcher(bot)
+rKeyBoard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+Button1 = KeyboardButton(text='/links')
+rKeyBoard.add(Button1)
+iKeyBoard = InlineKeyboardMarkup(row_width=2)
+iButton1 = InlineKeyboardButton(text='Google', url='https://www.google.com')
+iButton2 = InlineKeyboardButton(text='Yandex', url='https://www.yandex.ru')
+iKeyBoard.add([iButton1, iButton2])
+
+@dp.message_handler(commands=['start'])
+async def sendKeyBoard(message: types.Message):
+    await bot.send_message(chat_id=message.chat.id, text = 'Hello World', reply_markup=rKeyBoard)
+
+@dp.message_handler(commands=['links'])
+async def InlineSend(message: types.Message):
+    await bot.send_message(chat_id=message.chat.id, reply_markup=iKeyBoard)
+    await message.delete()
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
+
