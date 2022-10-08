c6eba5afec45f531777b5a0100d11988c0ce2a37
Andrey <13rav.pr@gmail.com>
2022-10-08 22:19:34 +0300

    fr4

--- First_big_project/main.py
+++ First_big_project/main.py
@@ -1,3 +1,17 @@
+from ast import main
 from aiogram import Bot, Dispatcher, executor, types
 
 from TOKEN import TOKEN_API
+
+
+bot = Bot(TOKEN_API)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def startCommand (message: types.Message):
+    await message.answer(text='Answer')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dispatcher=dp)
