401a8c090b448f9e01209d7e9cd756395db329c5
Andrey <13rav.pr@gmail.com>
2022-11-06 20:12:22 +0300

    rcfew

--- Lesson20.py
+++ Lesson20.py
@@ -8,22 +8,31 @@ async def EverySecond():
     global k
     k = 1
     while k < b:
-        await asyncio.Sleep(1)
-        print(f"Прошло {k} секунд")
+
+        await asyncio.sleep(1)
+
+        if k%3!=0:
+            print(f"Прошло {k} секунд")
         k=k+1
     return None
 
 async def EveryThreeSecond():
     global k
     while k < b:
-        await asyncio.Sleep(3)
-        rint(f"Прошло {k} секунд")
+
+        await asyncio.sleep(3)
+        
+        print("Прошло еще 3 секунды")
     return None
 
 async def main():
 
     task_1 = asyncio.create_task(EverySecond())
-    task_1 = asyncio.create_task(EveryThreeSecond())
+    task_2 = asyncio.create_task(EveryThreeSecond())
+
+    await task_1
+    await task_2
+
     return None
 
 asyncio.run(main())
