3a11a6e11ed00ebef7f45be2f8dc3f6cbb669c50
Andrew <13rav.pr@gmail.com>
2023-06-05 22:14:15 +0300

    starting

--- Big_project_areyouguy/main.py
+++ Big_project_areyouguy/main.py
@@ -6,6 +6,11 @@ from TOKEN import TOKEN_API
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
 
+
+globalDB = {
+    chat
+
+}
 members={}
 MemberId=[]
 MemberUsername = ""
@@ -14,18 +19,16 @@ reg_text = """зарегестрированы следующие участни
 
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
-    stringInfo = await bot.get_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)
-    await message.answer(stringInfo.user.username)
-    await bot.send_message(chat_id=message.from_user.id, text="введи команду /registration чтобы учавствовать в выборе")
+    await bot.send_message(chat_id=message.chat.id, text="введи команду /registration чтобы учавствовать в выборе")
 
 @dp.message_handler(commands=['registration'])
 async def reg_command(message: types.Message):
     member = await bot.get_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)
     MemberUsername = member.user.username
     MemberId.append(member.user.id)
-    await bot.send_message(text="erg "+MemberUsername+" fg "+str(MemberId), chat_id=message.from_user.id)
-    await bot.send_message(text=reg_text+f'\n он @{MemberUsername} вошел в чат', chat_id=message.from_user.id)
-    await bot.send_message(text=reg_text, chat_id=message.from_user.id)
+    await bot.send_message(text="erg "+MemberUsername+" fg "+str(MemberId), chat_id=message.chat.id)
+    await bot.send_message(text=reg_text+f'\n он @{MemberUsername} вошел в чат', chat_id=message.chat.id)
+    await bot.send_message(text=reg_text, chat_id=message.chat.id)
 
 
 if __name__ == '__main__':
---
+++ Big_project_areyouguy/testing.py
@@ -0,0 +1,19 @@
+
+chatID='0'
+GlobalDB = {
+    chatID:{
+        "memberID":[],
+        "memberIDgay":2,
+    },
+    "chatID2":{
+        "memberID":[],
+        "memberIDgay":0,
+    }
+}
+
+GlobalDB[chatID=chatID+3]="33"
+#GlobalDB['6545676']["memberID"].append(54456)
+#GlobalDB["6545676"]["memberID"].append(54455546)
+#GlobalDB["6545676"]["memberIDgay"] = GlobalDB["6545676"]["memberIDgay"]+True
+
+print(GlobalDB)
